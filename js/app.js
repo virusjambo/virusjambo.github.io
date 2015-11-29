221d4c9d4c34e01a28e0139ef0729c8bf89f1981
amit <virusjambo@gmail.com>
2015-11-29 20:38:11 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	
 	 $(document).ready(function(){
       $('.slider').slider({full_width: true,Height:"100%"});
+		     $(".button-collapse").sideNav();
+
     });
 	
 	
